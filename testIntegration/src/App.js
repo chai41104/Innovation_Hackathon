6b5659cdc985d5523f6159014ecb742002c56752
Hunterx7 <ialbetsram@gmail.com>
2017-11-26 14:06:58 +0000

    before testing

--- testIntegration/src/App.js
+++ testIntegration/src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react'
-import MinerContract from '../build/contracts/Miner.json'
-import getWeb3 from './utils/getWeb3'
+import React, { Component } from 'react';
+import MinerContract from '../build/contracts/Miner.json';
+import getWeb3 from './utils/getWeb3';
+import { FormGroup, ControlLabel, HelpBlock, FormControl } from 'react-bootstrap';
 
-import './css/oswald.css'
-import './css/open-sans.css'
-import './css/pure-min.css'
-import './App.css'
+import './css/oswald.css';
+import './css/open-sans.css';
+import './css/pure-min.css';
+import './App.css';
 
 class App extends Component {
   constructor(props) {
@@ -15,6 +16,10 @@ class App extends Component {
       storageValue: 0,
       web3: null
     }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.genID = this.genID.bind(this);
+    this.instantiateContract = this.instantiateContract.bind(this);
   }
 
   componentWillMount() {
@@ -35,14 +40,13 @@ class App extends Component {
     })
   }
 
-  instantiateContract() {
-    /*
-     * SMART CONTRACT EXAMPLE
-     *
-     * Normally these functions would be called in the context of a
-     * state management library, but for convenience I've placed them here.
-     */
+  genID() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+       .toString(16)
+       .substring(1);
+  }
 
+  instantiateContract() {
     const contract = require('truffle-contract')
     const miner = contract(MinerContract)
     miner.setProvider(this.state.web3.currentProvider)
@@ -50,22 +54,17 @@ class App extends Component {
     // Declaring this for later so we can chain functions on SimpleStorage.
     var minerStorageInstance
 
-    function genID() {
-      return Math.floor((1 + Math.random()) * 0x10000)
-         .toString(16)
-         .substring(1);
-    }
-
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
       miner.deployed().then((instance) => {
         minerStorageInstance = instance
 
         // Stores a given value, 5 by default.
-        var genId = genID();
+        var genId = this.genID;
         var guysName = "";
         var isVerif = true;
-        var proof = 
+        var proof = "";
+
 
         return minerStorageInstance.set(genId, guysName, isVerif, proof, {from: accounts[0]})
       }).then((result) => {
@@ -78,17 +77,85 @@ class App extends Component {
     })
   }
 
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+
+
   render() {
     return (
       <div className="App">
+
         <nav className="navbar pure-menu pure-menu-horizontal">
             <a href="#" className="pure-menu-heading pure-menu-link">GroundWorks</a>
+            <a href="#" className="pure-menu-link">Artisanal Minners Blockchain KYC</a>
+            <a href="#" className="pure-menu-link">Drone Sample Collection</a>
         </nav>
 
         <main className="container">
-
           <div className="pure-g">
             <div className="pure-u-1-1">
+            <section className="content">
+              <div className="box box-primary">
+              <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                <form role="form" onSubmit={this.handleSubmit}>
+                  <div className="box-body">
+                    <div className="form-group">
+                      <div className="radio">
+                        <label/>
+                          <input type="radio" name="optionsRadios" id="optionsRadios1" value={this.state.value} onChange={this.handleChange} checked />
+                          Passport
+                      </div>
+                      <div className="radio">
+                        <label />
+                          <input type="radio" name="optionsRadios" id="optionsRadios2" value={this.state.value} onChange={this.handleChange} />
+                          Valid ID
+                      </div>
+                      <div className="radio">
+                        <label/>
+                          <input type="radio" name="optionsRadios" id="optionsRadios3" value={this.state.value} onChange={this.handleChange} />
+                          Driver Licence
+                      </div>
+                    </div>
+
+                    <div className="form-group">
+                      <label for="exampleInputFile" value={this.state.value} onChange={this.handleChange} />Upload your document
+                      <input type="file" id="exampleInputFile" />
+                    </div>
+                    <div className="form-group">
+                      <div className="input-group">
+                          <span className="input-group-addon">@</span>
+                          <input type="text" className="form-control" placeholder="Your name" value={this.state.value} onChange={this.handleChange} />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="input-group">
+                        <span className="input-group-addon"><i className="fa fa-envelope"></i></span>
+                        <input type="email" className="form-control" placeholder="Your Email" value={this.state.value} onChange={this.handleChange} />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="input-group">
+                        <span className="input-group-addon"><i className="fa fa-phone"></i></span>
+                        <input type="email" className="form-control" placeholder="Your phonenumber" value={this.state.value} onChange={this.handleChange} />
+                      </div>
+                    </div>
+                    <div className="checkbox">
+                      <label />
+                        <input type="checkbox" /><label />I agree with <a href="#">licence</a> and I understand.
+                    </div>
+                  </div>
+                  <div className="box-footer">
+                    <a onclick="{this.instantiateContract}" className="btn btn-primary">Process</a>
+                  </div>
+                </form>
+              </div>
+              </div>
+              </div>
+            </section>
+
 
               <p>The stored value is: {this.state.storageValue}</p>
             </div>
